6c93e5b941414626706cd7debd7bc764c9af7804
sc-GiulianoFrau <giuliano.frau@smart-contact.it>
2024-09-27 14:28:46 +0200

    refact code

--- src/components/AppSkills.jsx
+++ src/components/AppSkills.jsx
@@ -2,7 +2,7 @@ import { skills } from "../options/skills";
 import SkillCard from "./SkillCard";
 const AppSkills = () => {
   return (
-    <div className="app__skills py-6">
+    <div className="app__skills py-8">
       <div className="app__skills__content">
         <h1 className="text-center">Le mie skills</h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4  justify-center">
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,9 +1,9 @@
 const SkillCard = ({ name, icon, description }) => {
   return (
-    <div className="skill-card text-white p-8 rounded-lg flex flex-col gap-3 ">
+    <div className="skill-card text-white p-8 sm:p-4 lg:p-8 rounded-lg flex flex-col gap-3 lg:h-72">
       <i className={icon + " text-5xl flex justify-center text-primary"}></i>
-      <h3 className="text-center text-2xl">{name}</h3>
-      <p className="text-center">{description}</p>
+      <h3 className="text-center text-2xl ">{name}</h3>
+      <p className="text-center mt-auto lg:mt-0 ">{description}</p>
     </div>
   );
 };
