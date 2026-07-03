7b66bd172a46d8922425f48ba0062892a7feb0ef
Giuliano Frau <giuliano.frau1992@gmail.com>
2026-07-03 15:29:27 +0200

    wip

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // Central place for personal links & assets.
 export const CV_URL =
-  "https://drive.google.com/file/d/1kwnF2kE5lwlUR1xNYFIriD6MeiN-Oc4x/view?usp=drive_link";
+  "https://drive.google.com/file/d/169Fo8J34YXTWX2v-R9UvklLt7XoxVHHs/view?usp=drive_link";
 
 export const SOCIALS = {
   linkedin: "https://www.linkedin.com/in/giuliano-frau-8728a61bb/",
